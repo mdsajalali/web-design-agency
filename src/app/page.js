8cc5f26396873e7ac62cee4521d54a4aece71332
mdsajalali <mdsajalali2020@gmail.com>
2023-09-26 17:36:55 +0600

    navbar section create

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="bg-[#D7F5DC] ">
+      <div className="max-w-[1440px] mx-auto p-4">
+        <nav className="md:flex items-center justify-between ">
+          <div className="w-44">
+            <Link href="/">
+              <Image
+                src="/images/DesignAGENCY.png"
+                width={500}
+                height={500}
+                alt="Picture of the author"
+              />
+            </Link>
+          </div>
+          <div className="flex items-center justify-center gap-5 text-[15px] font-medium ">
+            <Link
+              href="/"
+              className="hover:text-[#20B15A] transition ease-in-out delay-150"
+            >
+              Home
+            </Link>
+            <Link
+              href="/"
+              className="hover:text-[#20B15A] transition ease-in-out delay-150"
+            >
+              Team
+            </Link>
+            <Link
+              href="/"
+              className="hover:text-[#20B15A] transition ease-in-out delay-150"
+            >
+              Service
+            </Link>
+            <Link
+              href="/"
+              className="hover:text-[#20B15A] transition ease-in-out delay-150"
+            >
+              Projects
+            </Link>
+            <Link
+              href="/"
+              className="hover:text-[#20B15A] transition ease-in-out delay-150"
+            >
+              Testimonials
+            </Link>
 
-export default Navbar
+            <div className="border border-[#20B15A] px-3 py-2 rounded-lg hover:bg-[#20B15A] hover:text-white transition ease-in-out delay-150 cursor-pointer">
+              <Link href="/">Login</Link>
+            </div>
+            <div className="bg-[#20B15A] text-white px-3 py-2 rounded-lg cursor-pointer">
+              <Link href="/">Register</Link>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
