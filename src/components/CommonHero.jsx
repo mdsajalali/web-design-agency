470c6e43eccdd4a40971c416d92042bc02a95ef9
mdsajalali <mdsajalali2020@gmail.com>
2023-09-29 23:11:38 +0600

    common hero page bg added

--- src/components/CommonHero.jsx
+++ src/components/CommonHero.jsx
@@ -3,18 +3,20 @@ import { TbMathGreater } from "react-icons/tb";
 
 const CommonHero = ({ heading, nav, currentNav }) => {
   return (
-    <div className="max-w-[1440px] h-[200px] mx-auto p-4 px-5 md:px-20 flex flex-col items-start justify-center ">
-      <h1 className="text-4xl font-semibold mb-3">{heading}</h1>
+   <div className="bg-[#F8F9FC] h-[280px]">
+     <div className="max-w-[1440px] mx-auto p-4 px-5 md:px-20  flex flex-col items-start justify-center ">
+      <h1 className="text-4xl font-semibold mb-3 mt-32">{heading}</h1>
       <div className="flex gap-2 items-center justify-center">
-        <Link className="font-medium" href="/">{nav}</Link>
+        <Link className="font-medium" href="/">
+          {nav}
+        </Link>
         <span>
           <TbMathGreater />
         </span>
-        <span className="text-[#20B15A]" >
-          {currentNav}
-        </span>
+        <span className="text-[#20B15A]">{currentNav}</span>
       </div>
     </div>
+   </div>
   );
 };
 
