492e7bacc86331208fc3c74dbf31752cb5b5bf93
mdsajalali <mdsajalali2020@gmail.com>
2023-09-29 17:40:32 +0600

    all services section update

--- src/components/AllServices.jsx
+++ src/components/AllServices.jsx
@@ -12,230 +12,199 @@ const AllServices = () => {
         </h1>
       </div>
 
-      {/* <div>
-        <div className="grid grid-cols-6 gap-4">
-          <div className="col-start-1 col-span-3">
-            <Image
-              src="/images/sercie-img1.jpeg"
-              width={300}
-              height={300}
-              alt="Picture of the author"
-            />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-10">
+        <div className="p-5 shadow-lg rounded-xl">
+          <div className="text-2xl font-semibold uppercase">
+            Build & Launch without problems
           </div>
-          <div className="">
-            <Image
-              src="/images/sercie-img1.jpeg"
-              width={300}
-              height={300}
-              alt="Picture of the author"
-            />
+          <div className="text-gray-400 py-4 text-[15px]">
+            Increase your teams productivity and save time with a bot that
+            answers and fields customer inquiries
+          </div>
+          <div className="grid grid-cols-3 gap-4  ">
+            <div className="w-full h-[240px] col-span-2">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className="  h-full  rounded-lg "
+              />
+            </div>
+            <div className="w-full h-[240px]">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className=" h-full rounded-lg"
+              />
+            </div>
+            <div className="w-full h-[240px] col-auto">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className="  h-full  rounded-lg "
+              />
+            </div>
+            <div className="w-full h-[240px] col-auto">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className=" h-full rounded-lg"
+              />
+            </div>
           </div>
         </div>
-        </div> */}
 
-<div className="grid grid-cols-1 md:grid-cols-2">
-   <div className="p-5 shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-xl">
-        <div className="text-[26px] font-semibold uppercase">
-          Build & Launch without problems
-        </div>
-        <div className="text-[#9D9D9D] py-4 text-[16px]">
-          Increase your teams productivity and save time with a bot that answers
-          and fields customer inquiries
-        </div>
-        <div className="grid grid-cols-12 gap-4 [&>:first-child]:col-span-8 [&>:nth-child(2)]:col-span-4 [&>]:col-span-6 [&>]:rounded-xl">
-          <div className="w-full h-[240px] bg-gray-100 overflow-hidden">
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
+        <div className="p-5 shadow-lg rounded-xl">
+          <div className="text-2xl font-semibold uppercase">
+            Build & Launch without problems
           </div>
-          <div className="w-full h-[240px] bg-gray-500">
-            {" "}
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
+          <div className="text-gray-400 py-4 text-[15px]">
+            Increase your teams productivity and save time with a bot that
+            answers and fields customer inquiries
           </div>
-          <div className="w-full h-[240px] bg-gray-500">
-            {" "}
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="w-full h-[240px] bg-gray-200">
-            
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
+          <div className="grid grid-cols-3 gap-4  ">
+            <div className="w-full h-[240px] col-span-2">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className="  h-full  rounded-lg "
+              />
+            </div>
+            <div className="w-full h-[240px]">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className=" h-full rounded-lg"
+              />
+            </div>
+            <div className="w-full h-[240px] col-auto">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className="  h-full  rounded-lg "
+              />
+            </div>
+            <div className="w-full h-[240px] col-auto">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className=" h-full rounded-lg"
+              />
+            </div>
           </div>
         </div>
-      </div>
-   <div className="p-5 shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-xl">
-        <div className="text-[26px] font-semibold uppercase">
-          Build & Launch without problems
-        </div>
-        <div className="text-[#9D9D9D] py-4 text-[16px]">
-          Increase your teams productivity and save time with a bot that answers
-          and fields customer inquiries
-        </div>
-        <div className="grid grid-cols-12 gap-4 [&>:first-child]:col-span-8 [&>:nth-child(2)]:col-span-4 [&>]:col-span-6 [&>]:rounded-xl">
-          <div className="w-full h-[240px] bg-gray-100 overflow-hidden">
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
+
+        <div className="p-5 shadow-lg rounded-xl">
+          <div className="text-2xl font-semibold uppercase">
+            Build & Launch without problems
           </div>
-          <div className="w-full h-[240px] bg-gray-500">
-            {" "}
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
+          <div className="text-gray-400 py-4 text-[15px]">
+            Increase your teams productivity and save time with a bot that
+            answers and fields customer inquiries
           </div>
-          <div className="w-full h-[240px] bg-gray-500">
-            {" "}
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="w-full h-[240px] bg-gray-200">
-            
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
+          <div className="grid grid-cols-3 gap-4  ">
+            <div className="w-full h-[240px] col-span-2">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className="  h-full  rounded-lg "
+              />
+            </div>
+            <div className="w-full h-[240px]">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className=" h-full rounded-lg"
+              />
+            </div>
+            <div className="w-full h-[240px] col-auto">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className="  h-full  rounded-lg "
+              />
+            </div>
+            <div className="w-full h-[240px] col-auto">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className=" h-full rounded-lg"
+              />
+            </div>
           </div>
         </div>
-      </div>
-   <div className="p-5 shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-xl">
-        <div className="text-[26px] font-semibold uppercase">
-          Build & Launch without problems
-        </div>
-        <div className="text-[#9D9D9D] py-4 text-[16px]">
-          Increase your teams productivity and save time with a bot that answers
-          and fields customer inquiries
-        </div>
-        <div className="grid grid-cols-12 gap-4 [&>:first-child]:col-span-8 [&>:nth-child(2)]:col-span-4 [&>]:col-span-6 [&>]:rounded-xl">
-          <div className="w-full h-[240px] bg-gray-100 overflow-hidden">
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
+
+        <div className="p-5 shadow-lg rounded-xl">
+          <div className="text-2xl font-semibold uppercase">
+            Build & Launch without problems
           </div>
-          <div className="w-full h-[240px] bg-gray-500">
-            {" "}
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
+          <div className="text-gray-400 py-4 text-[15px]">
+            Increase your teams productivity and save time with a bot that
+            answers and fields customer inquiries
           </div>
-          <div className="w-full h-[240px] bg-gray-500">
-            {" "}
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="w-full h-[240px] bg-gray-200">
-            
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
+          <div className="grid grid-cols-3 gap-4  ">
+            <div className="w-full h-[240px] col-span-2">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className="  h-full  rounded-lg "
+              />
+            </div>
+            <div className="w-full h-[240px]">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className=" h-full rounded-lg"
+              />
+            </div>
+            <div className="w-full h-[240px] col-auto">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className="  h-full  rounded-lg "
+              />
+            </div>
+            <div className="w-full h-[240px] col-auto">
+              <Image
+                src="/images/sercie-img1.jpeg"
+                alt="image"
+                height={500}
+                width={500}
+                className=" h-full rounded-lg"
+              />
+            </div>
           </div>
         </div>
       </div>
-   <div className="p-5 shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-xl">
-        <div className="text-[26px] font-semibold uppercase">
-          Build & Launch without problems
-        </div>
-        <div className="text-[#9D9D9D] py-4 text-[16px]">
-          Increase your teams productivity and save time with a bot that answers
-          and fields customer inquiries
-        </div>
-        <div className="grid grid-cols-12 gap-4 [&>:first-child]:col-span-8 [&>:nth-child(2)]:col-span-4 [&>]:col-span-6 [&>]:rounded-xl">
-          <div className="w-full h-[240px] bg-gray-100 overflow-hidden">
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="w-full h-[240px] bg-gray-500">
-            {" "}
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="w-full h-[240px] bg-gray-500">
-            {" "}
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="w-full h-[240px] bg-gray-200">
-            
-            <Image
-              src="/images/sercie-img1.jpeg"
-              alt="image"
-              height={500}
-              width={500}
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
-      </div>
-</div>
-     
     </div>
   );
 };
